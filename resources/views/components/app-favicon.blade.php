b5fc13d37cf762d27323f38f6e51f2ff5430395c
Pacho <93363437+BiaHoi-BaChien@users.noreply.github.com>
2025-11-28 09:26:54 +0700

    Ensure favicon.svg is used (#96)

--- resources/views/components/app-favicon.blade.php
+++ resources/views/components/app-favicon.blade.php
@@ -4,3 +4,4 @@
     $faviconPath = $prefix === '' ? 'favicon.svg' : $prefix.'/favicon.svg';
 @endphp
 <link rel="icon" type="image/svg+xml" href="{{ $baseUrl.'/'.$faviconPath }}">
+<link rel="shortcut icon" type="image/svg+xml" href="{{ $baseUrl.'/'.$faviconPath }}">
